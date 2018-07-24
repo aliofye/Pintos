9496577122446d1e82be53ad1e4736d85abbc17b
os-class <henryboj@bu.edu>
2018-07-24 10:51:38 -0400

    added some implementation to process.c, looking to review it as well

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -4,7 +4,7 @@
 #include <round.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <string.h> 
 #include "userprog/gdt.h"
 #include "userprog/pagedir.h"
 #include "userprog/tss.h"
@@ -25,23 +25,52 @@ static bool load (const char *cmdline, void (**eip) (void), void **esp);
    FILENAME.  The new thread may be scheduled (and may even exit)
    before process_execute() returns.  Returns the new process's
    thread id, or TID_ERROR if the thread cannot be created. */
+
+
 tid_t
 process_execute (const char *file_name) 
+
+//changes needed for part 1 of project
+//call helper function to break up the input -> doesn't make sense
 {
+
+
   char *fn_copy;
-  tid_t tid;
+  tid_t tid; 
 
   /* Make a copy of FILE_NAME.
      Otherwise there's a race between the caller and load(). */
   fn_copy = palloc_get_page (0);
+
   if (fn_copy == NULL)
     return TID_ERROR;
+
   strlcpy (fn_copy, file_name, PGSIZE);
 
   /* Create a new thread to execute FILE_NAME. */
-  tid = thread_create (file_name, PRI_DEFAULT, start_process, fn_copy);
+  //MARCOOO!*!*!!*
+
+  char ** pointer; //gets passed into pointer 
+  char * newFileName= (char *) file_name; //strtok_r breaks it up but it needs be changed from a char *
+  char * brokenUp = strtok_r(newFileName, " ", pointer); 
+  //strtok_r needs string.h
+
+  
+  //file name is split up into strings but split by spaces
+  //while(file_name!=NULL)
+  //{
+
+  //}
+  //POLOOO!!*!*!*!*
+
+  tid = thread_create (brokenUp, PRI_DEFAULT, start_process, fn_copy);
+
   if (tid == TID_ERROR)
     palloc_free_page (fn_copy); 
+
+  //hex_dump(); //for testing to see if the output for part 1 is correct 
+
+
   return tid;
 }
 
